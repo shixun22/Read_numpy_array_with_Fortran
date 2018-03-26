c1740af7e1bc0cafd2001b093efebf90c99db1da
Xun Shi <x.straybird@gmail.com>
2018-03-26 10:57:56 +0200

    Update gen_fortran_readable_array.py
    Use "ravel" instead of "asfortranarray" or "reshape(shape, order='F')" to keep ND array in correct order!

--- gen_fortran_readable_array.py
+++ gen_fortran_readable_array.py
@@ -5,7 +5,10 @@ NY = 4
 NZ = 2
 
 a = np.random.rand(NX, NY, NZ)
-a.reshape((NX, NY, NZ), order='F').tofile('numpyarray_NX'+str(NX)+'_NY'+str(NY)+'_NZ'+str(NZ)+'.dat')
+a.ravel(order='F').tofile('numpyarray_NX'+str(NX)+'_NY'+str(NY)+'_NZ'+str(NZ)+'.dat')  
+
+# Note: neither "a.reshape((NX,NY,NZ), order='F')" nor "np.asfortranarray(a)" in python-2.7 keeps the ND array in correct order! 
+# Must use a.ravel first! 
 
 print 'Done!'
 
